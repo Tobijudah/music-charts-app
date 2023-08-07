2d3727da7d0d93f80232c868e62837603401956c
Oluwatobiju Judah <tobijudah@gmail.com>
2023-08-07 14:14:41 +0100

    chore: updates to use internal serverless function as API

--- js/musixmatch.js
+++ js/musixmatch.js
@@ -1,6 +1,5 @@
 class Musixmatch {
    constructor(number, chart, country) {
-      this.apiKey = 'c93b979443af33c9e9f88812446b2eab'
       this.number = number
       this.chart = chart
       this.country = country 
@@ -9,7 +8,7 @@ class Musixmatch {
    async fetchData() {
    
       try {
-         const response = await fetch('https://chartssss.vercel.app/api/musixmatch', {
+         const response = await fetch('api/musixmatch', {
             method: 'POST',
             body: JSON.stringify({ chart: this.chart.split('.')[1], country: this.country, number: this.number }),
           })
@@ -24,6 +23,3 @@ class Musixmatch {
  
    }
 }
-
-
-// http://api.musixmatch.com/ws/1.1/chart.tracks.get?apikey=c93b979443af33c9e9f88812446b2eab&chart_name=top&page=1&page_size=5&country=us&f_has_lyrics=1
